dbbf8ef4daf8798be185d3853ed5a8ae1320724c
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-04-04 22:13:07 -0300

    login page implement

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage/login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
