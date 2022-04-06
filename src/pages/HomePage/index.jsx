109029f47dbb5a432ff2aa39b3eeedbc25cca390
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-04-05 22:20:34 -0300

    integration of  button logout into the application

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import { AuthContext } from "../../contexts/auth";
 
 const HomePage = () => {
-    return <div>HomePage</div>;
+    const { authenticated ,logout } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logout();
+
+    }
+
+    return (
+        <>
+    
+    <div>HomePage</div>
+    <p>{String(authenticated)}</p>
+    <button onClick={handleLogout}>LOGOUT</button>
+    </>
+    );
     };
 
 export default HomePage;
